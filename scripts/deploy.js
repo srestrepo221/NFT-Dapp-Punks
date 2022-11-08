496e8b91844ef3c60db2f3a44acc288e658757e7
santiago restrepo <srest031@fiu.edu>
2022-11-08 16:15:21 -0500

    Deploys NFT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,16 +7,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const NAME = 'Dapp Punks'
+  const SYMBOL = 'DP'
+  const COST = ethers.utils.parseUnits('10', 'ether')
+  const MAX_SUPPLY = 25
+  const NFT_MINT_DATE = (Date.now() + 60000).toString().slice(0, 10)
+  const IPFS_METADATA_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
 
-  // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  // Deploy NFT
+  const NFT = await hre.ethers.getContractFactory('NFT')
+  let nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, NFT_MINT_DATE, IPFS_METADATA_URI)
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await nft.deployed()
+  console.log(`NFT deployed to: ${nft.address}\n`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
